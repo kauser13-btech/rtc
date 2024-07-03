2e7483359f1b37267c110009331fbf208f24bebf
Nilove <kauser.nilove@sayvee.com>
2024-07-03 18:56:23 +0600

    Fix and Final

--- src/Wrapper.js
+++ src/Wrapper.js
@@ -10,7 +10,7 @@ const turnServerConfig = {
 // stun:stun.l.google.com:19302
 const configuration = {
     iceServers: [
-        { urls: 'stun:stun.l.google.com:19302' },turnServerConfig
+        turnServerConfig
     ],
     iceCandidatePoolSize: 10,
 };
